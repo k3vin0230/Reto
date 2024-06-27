c9e3b451c4431fca2827e6ab9a7254cd9a021110
k3vin0230 <kevintrujillo1883@gmail.com>
2024-06-26 20:58:15 -0600

    Ver Productos de DummyJson

--- routes/admins.js
+++ routes/admins.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+
+// URL de la API de DummyJSON para los productos
+const DUMMYJSON_API = 'https://dummyjson.com/products';
+
+// Función para obtener datos de productos de DummyJSON
+async function BuscarproductosDummy() {
+  try {
+      const response = await axios.get(DUMMYJSON_API);
+      return response.data;
+  } catch (error) {
+      console.error('Error al obtener productos de DummyJSON:', error.message);
+      throw error;
+  }
+}
+
+let products = [];
 
 
-router.get('/', function(req, res, next) {
- 
+// Ruta para obtener todos los productos
+router.get('/products', async (req, res) => {
+  try {
+      // Obtener datos de productos desde DummyJSON
+      products = await BuscarproductosDummy();
+      res.json(products);
+  } catch (error) {
+      res.status(500).json({ message: 'Error al obtener productos desde DummyJSON' });
+  }
 });
-
 module.exports = router;
