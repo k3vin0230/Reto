6f02f120fd605d96c4076f7526421b62bee42ff3
k3vin0230 <kevintrujillo1883@gmail.com>
2024-06-26 21:15:28 -0600

    Crear Producto y Middleware para verificar JWT y si es Administrador

--- routes/admins.js
+++ routes/admins.js
@@ -16,6 +16,7 @@ async function BuscarproductosDummy() {
   }
 }
 
+
 let products = [];
 
 
@@ -29,4 +30,54 @@ router.get('/products', async (req, res) => {
       res.status(500).json({ message: 'Error al obtener productos desde DummyJSON' });
   }
 });
+
+// Ruta para crear un nuevo producto (solo para administradores)
+router.post('/products', ValidarTokenyAdmin, async (req, res) => {
+  const { title, price } = req.body;
+  try {
+      // Crear un nuevo producto en DummyJSON
+      const newProduct = { title, price };
+      const response = await axios.post(DUMMYJSON_API, newProduct);
+      products.push(response.data);
+      res.status(201).json(response.data);
+  } catch (error) {
+      res.status(500).json({ message: 'Error al crear un nuevo producto en DummyJSON' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+// Middleware para verificar el token JWT y el rol de administrador
+const JWT_SECRET = 'cl4v3';
+function ValidarTokenyAdmin(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+      return res.status(401).json({ message: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(403).json({ message: 'Token inválido' });
+      }
+
+      // Verificar si el usuario es administrador
+      if (decoded.role !== 'admin') {
+          return res.status(403).json({ message: 'Acceso no autorizado para crear productos' });
+      }
+
+      // Almacenar la información decodificada del usuario para su uso posterior si es necesario
+      req.user = decoded;
+      next();
+  });
+}
+
 module.exports = router;
